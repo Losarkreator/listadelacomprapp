f296d7597064ec1b21d5d67d129a004cb0841355
Losarkreator <losarkreator@gmail.com>
2023-05-26 17:17:38 +0200

    primera version

--- listadelacomprapp/Views/ContentView.swift
+++ listadelacomprapp/Views/ContentView.swift
@@ -6,14 +6,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var products = ["Manzanas", "Plátanos", "Leche", "Pan"]
+    @State private var newProduct = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello!")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(0..<products.count, id: \.self) { index in
+                        HStack {
+                            Button(action: {
+                                self.toggleProduct(at: index)
+                            }) {
+                                Image(systemName: self.products[index].contains("✓") ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(.blue)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            Text(self.products[index])
+                        }
+                    }
+                    .onDelete(perform: deleteProduct)
+                }
+                .navigationBarTitle("Lista de Compra")
+                
+                HStack {
+                    TextField("Nuevo producto", text: $newProduct)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button(action: {
+                        self.addProduct()
+                    }) {
+                        Text("Añadir")
+                    }
+                }
+                .padding()
+            }
         }
-        .padding()
+    }
+    
+    func toggleProduct(at index: Int) {
+        if products[index].contains("✓") {
+            products[index] = products[index].replacingOccurrences(of: "✓ ", with: "")
+        } else {
+            products[index] = "✓ " + products[index]
+        }
+    }
+    
+    func deleteProduct(at offsets: IndexSet) {
+        products.remove(atOffsets: offsets)
+    }
+    
+    func addProduct() {
+        if !newProduct.isEmpty {
+            products.append(newProduct)
+            newProduct = ""
+        }
     }
 }
 
